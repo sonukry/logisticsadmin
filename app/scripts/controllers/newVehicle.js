ff6c2ff0aaf7af19813a830021a4f74c42ad643b
Shubham Verma <shubham.verman@gmail.com>
2014-08-31 16:02:54 +0530

    Updated vehicle frnt-end to select device while adding a new vehicle

--- app/scripts/controllers/newVehicle.js
+++ app/scripts/controllers/newVehicle.js
@@ -3,18 +3,17 @@
 angular.module('angularPassportApp')
   .controller('NewVehicleCtrl', function ($scope, $location, $cookieStore, Fleets) {
         
-    Fleets.get()
-    .success(function(data) {
-      $scope.fleets = data;
-      
-      $scope.currentFleet = $cookieStore.get('fleetId');
-      $scope.currentFleetName = $cookieStore.get('fleetName');
-    })
-    .error(function(data) {});
+    $scope.currentFleet = $cookieStore.get('fleetId');
+    $scope.currentFleetName = $cookieStore.get('fleetName');
     
     $scope.vehicle = {};
     
     $scope.createVehicle = function() {
+      
+      $scope.vehicle.device = {
+        _id: $scope.vehicle.device.split(',')[0],
+        uuid: $scope.vehicle.device.split(',')[1]
+      };
       
       Fleets.addVehicle($scope.currentFleet, {vehicle: $scope.vehicle})
       .success(function(data) {
@@ -25,5 +24,10 @@ angular.module('angularPassportApp')
       });
       
     };
-    
+
+    Fleets.getDevices($scope.currentFleet)
+    .success(function(data) {
+      $scope.devices = data.devices;
+    })
+    .error(function(data) {});
   });
